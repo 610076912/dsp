3824131e74b4959d7e4f83b0341b75a00b6252fb
Abeast <772325766@qq.com>
2017-09-04 14:18:48 +0800

    首页

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,23 @@
     <div class="header">
       <div class="header-top">
         <div class="center">
-
+          <div class="header-welcome">欢迎您，<span class="header-ggz">广告主</span><span class="header-name">{{name}} <i class="el-icon-caret-bottom"></i></span><span class="logout">安全退出</span></div>
         </div>
       </div>
       <div class="header-con">
         <div class="center">
+          <div class="logo"><router-link to="/"></router-link></div>
           <ul>
-            <li><router-link to="/">首页</router-link></li>
-            <li><router-link to="plan">推广数据</router-link></li>
-            <li><router-link to="data">数据监测</router-link></li>
+            <li><router-link to="/" >首页</router-link></li>
+            <li><router-link to="plan" >推广数据</router-link></li>
+            <li><router-link to="data" >数据监测</router-link></li>
             <li><router-link to="finance">财务管理</router-link></li>
           </ul>
         </div>
       </div>
       <div class="header-tab">
         <div class="center">
-
+          <div class="location">{{location}}</div>  
         </div>
       </div>
     </div>
@@ -26,14 +27,28 @@
       <router-view></router-view>
     </div>
     <div class="footer">
-      页尾
+      <div class="center">
+        页面底部
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      name: '电风扇',
+      path: [true, false, false, false],
+      location: '首页'
+    }
+  },
+  methods: {
+    onActive: function () {
+      console.log(this.name)
+    }
+  }
 }
 
 </script>
@@ -63,6 +78,26 @@ export default {
     height: 40px;
     background: #454546;
   }
+  .header-welcome{
+    float: right ;
+    padding: 10px 0;
+    line-height: 20px;
+    color: #fff;
+  }
+  .header-ggz{
+    display: inline-block;
+    background: #1D8CE0;
+    padding: 0 5px;
+    margin: 0 3px;
+    border-radius: 3px;
+  }
+  .header-name{
+    margin: 0 5px 0 3px;
+    cursor: pointer;
+  }
+  .logout{
+    cursor: pointer;
+  }
   .header-con{
     width: 100%;
     height: 70px;
@@ -73,6 +108,16 @@ export default {
     height: 50px;
     background: #f9f9f9;
   }
+  .location{
+    line-height: 50px; 
+    font-weight: bold;
+    display block;
+    height: 48px;
+    width 80px;
+    text-align: center;
+    font-size 14px;
+    border-bottom 2px solid #1D8CE0;
+  }
   .header-con ul{
     width: 560px;
     height: 100%;
@@ -82,10 +127,21 @@ export default {
     width: 140px;
     height: 100%;
     float: left;
+    font-size 14px;
     text-align: center;
     line-height: 70px;
     cursor: pointer;
   }
+  .header-con ul li .active{
+    color: #1D8CE0;
+  }
+  .logo{
+    height: 100%;
+    width: 260px;
+    background: gray;
+    float left
+  }
+  
 
   .main{
     width: 1200px;
--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,5 +1,170 @@
 <template>
-    <div>index.vue</div>
+    <div class="index">
+      <div class="now-location"><b>当前位置：</b>{{location}}</div>
+      <div class="index-content">
+        <div class="user">
+          <div class="user-header"></div>
+          <div class="user-info-right">
+            <div class="user-info"> 欢迎您，{{name}} <span class="self-space">个人中心</span></div>
+            <p>推广余额：<span class="color-red">￥{{sum}}</span></p>
+            <div class="link-button">
+              <el-button size="small" type="primary">进入推广计划</el-button>
+              <el-button size="small">新建活动</el-button>
+            </div>  
+          </div>
+        </div>
+        <div class="statist">
+          <ul class="tab-tag">
+            <li v-for="(key,index) in tags" v-bind:class="{tagActive:(indexs == index)}" @click="toggleTags(index)">{{key}}</li>
+          </ul>
+          <div class="tag-items">
+            <div class="tag-item" v-for="(key,index) in datas" v-if="indexs == index">
+                 
+            <el-row class="count-data">
+              <el-col :span="6"><div class="grid-content r b"><span>0</span><p>今日曝光量</p></div></el-col>
+              <el-col :span="6"><div class="grid-content r b"><span>0</span><p>今日点击量</p></div></el-col>
+              <el-col :span="6"><div class="grid-content r b"><span>0</span><p>今日平均点击率</p></div></el-col>
+              <el-col :span="6"><div class="grid-content b"><span>0</span><p>今日消费</p></div></el-col>
+              <el-col :span="6"><div class="grid-content r"><span>0</span><p>正在投放</p></div></el-col>
+              <el-col :span="6"><div class="grid-content r"><span>0</span><p>待审核</p></div></el-col>
+              <el-col :span="6"><div class="grid-content r"><span>0</span><p>审核拒绝</p></div></el-col>
+              <el-col :span="6"><div class="grid-content"><span>0</span><p>今天曝光量</p></div></el-col>
+            </el-row>     
+              
+            </div>
+          </div>
+        </div>
+      </div> 
+      <!-- mobile -->
+      <div class="mobile">
+        <div class="mobile-title"><span>移动营销概况</span></div>
+        <div class="mobile-number">
+          <el-col :span="6"><div class="mobile-n-grid r"><span>0</span><p>曝光量（次）</p></div></el-col>
+          <el-col :span="6"><div class="mobile-n-grid r"><span>0</span><p>点击量（次）</p></div></el-col>
+          <el-col :span="6"><div class="mobile-n-grid r"><span>0</span><p>平均点击率（%）</p></div></el-col>
+          <el-col :span="6"><div class="mobile-n-grid"><span>0</span><p>花费（元）</p></div></el-col>
+        </div>
+        <div class="mobile-chart">
+          <div class="mobile-chart-color">
+            <div class="mobile-chart-color-l">
+              <span></span>
+              <template>
+                <el-select v-model="value" placeholder="曝光量">
+                  <!-- <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                  </el-option> -->
+                </el-select>
+              </template>
+            </div>
+            <div class="mobile-chart-color-r">
+              <span></span>
+              <template>
+                <el-select v-model="value" placeholder="点击量">
+                  <!-- <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                  </el-option> -->
+                </el-select>
+              </template>
+            </div>
+          </div>
+          <div class="mobile-chart-box">
+            
+          </div>
+        </div>
+      </div>
+      <!-- PC -->
+      <div class="mobile">
+        <div class="mobile-title"><span>PC营销概况</span></div>
+        <div class="mobile-number">
+          <el-col :span="6"><div class="mobile-n-grid r"><span>0</span><p>曝光量（次）</p></div></el-col>
+          <el-col :span="6"><div class="mobile-n-grid r"><span>0</span><p>点击量（次）</p></div></el-col>
+          <el-col :span="6"><div class="mobile-n-grid r"><span>0</span><p>平均点击率（%）</p></div></el-col>
+          <el-col :span="6"><div class="mobile-n-grid"><span>0</span><p>花费（元）</p></div></el-col>
+        </div>
+        <div class="mobile-chart">
+          <div class="mobile-chart-color">
+            <div class="mobile-chart-color-l">
+              <span></span>
+              <template>
+                <el-select v-model="value" placeholder="曝光量">
+                  <!-- <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                  </el-option> -->
+                </el-select>
+              </template>
+            </div>
+            <div class="mobile-chart-color-r">
+              <span></span>
+              <template>
+                <el-select v-model="value" placeholder="点击量">
+                  <!-- <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                  </el-option> -->
+                </el-select>
+              </template>
+            </div>
+          </div>
+          <div class="mobile-chart-box">
+            
+          </div>
+        </div>
+      </div>
+      <!-- OTT -->
+      <div class="mobile">
+        <div class="mobile-title"><span>OTT营销概况</span></div>
+        <div class="mobile-number">
+          <el-col :span="6"><div class="mobile-n-grid r"><span>0</span><p>曝光量（次）</p></div></el-col>
+          <el-col :span="6"><div class="mobile-n-grid r"><span>0</span><p>点击量（次）</p></div></el-col>
+          <el-col :span="6"><div class="mobile-n-grid r"><span>0</span><p>平均点击率（%）</p></div></el-col>
+          <el-col :span="6"><div class="mobile-n-grid"><span>0</span><p>花费（元）</p></div></el-col>
+        </div>
+        <div class="mobile-chart">
+          <div class="mobile-chart-color">
+            <div class="mobile-chart-color-l">
+              <span></span>
+              <template>
+                <el-select v-model="value" placeholder="曝光量">
+                  <!-- <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                  </el-option> -->
+                </el-select>
+              </template>
+            </div>
+            <div class="mobile-chart-color-r">
+              <span></span>
+              <template>
+                <el-select v-model="value" placeholder="点击量">
+                  <!-- <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                  </el-option> -->
+                </el-select>
+              </template>
+            </div>
+          </div>
+          <div class="mobile-chart-box">
+            
+          </div>
+        </div>
+      </div>     
+    </div>
 </template>
 
 <script>
@@ -7,12 +172,197 @@
       name: 'index',
       data () {
         return {
-          msg: 'index'
+          value: '',
+          name: '电风扇',
+          msg: 'index',
+          location: '首页',
+          sum: 500,
+          tags: ['移动', 'PC', 'OTT'],
+          indexs: 0,
+          datas: ['我是移动页面', 'pc页面在此', '发生的离开']
+        }
+      },
+      methods: {
+        toggleTags: function (index) {
+          this.indexs = index
         }
       }
     }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" type="text/stylus">
+  .index{
+    .index-content{
+      width 100%;
+      height 200px;
+      border 1px solid #e1e1e1;
+      border-radius 3px;
+      padding 30px;
+      background #fff;
+    }
+    .user{
+      height 100%;
+      width 400px;
+      float left;
+    }
+    .user-info-right{
+      height 100%;
+      width 270px;
+      float right;
+    }
+    .user-header{
+      height 100px;
+      width 100px;
+      background #ddd;
+      border-radius: 50px;
+      float left;
+    }
+    .now-location{
+      height 68px;
+      line-height 68px;
+    }
+    .user-info{
+      height 50px;
+      line-height 50px;
+      width 100%;
+      font-size 14px
+    }
+    .self-space{
+      color #1D8CE0;
+      float right;
+    }
+    .link-button{
+      margin-top 20px
+    }
+    .tab-tag{
+      height 100%;
+      width 60px;
+      float left;
+      .tagActive{
+        background #1D8CE0;
+        color #fff;
+      }
+    }
+    .tab-tag li{
+      display block;
+      height 46px;
+      line-height 46px;
+      text-align center;
+      cursor pointer;
+      background #f9f9f9;
+    }
+    .tag-items{
+      float right;
+      height 100%;
+      width 638px;
+      border-left  1px solid #ccc
+    }
+    .tag-item{
+      height 100%;
+      width 100%;
+    }
 
+    .statist{
+      float right;
+      border 1px solid #ccc;
+      border-radius 3px;
+      height 100%;
+      width 700px;
+      overflow hidden
+    }
+    .count-data{
+      width 100%;
+      height 100%  
+    }
+    .grid-content{
+      height 69px; 
+      text-align center
+      padding 12px 0
+      span{
+        font-size 20px
+      }
+    }
+    .mobile{
+      width 100%
+      margin 20px 0 20px 0
+      background #fff
+      border-radius 3px
+      border 1px solid #e1e1e1
+      overflow hidden
+    }
+    .mobile-number{
+      height 130px
+      text-align center
+      border-bottom 1px solid #e1e1e1
+    }
+    .mobile-title{
+      height 34px
+      line-height 34px
+      border-bottom 1px solid #e1e1e1
+      background #f4f5f9 
+    }
+    .mobile-title span{
+      margin-left 10px
+    }
+    
+    .mobile-n-grid{
+      height 130px
+      padding 40px 0;
+      span{
+        font-size 26px
+        font-weight bold
+        margin-top 20px
+      }
+    }
+    
+
+    .mobile-chart{
+      padding: 30px
+    }
+    .mobile-chart-color{
+      width 100%
+      height 30px;
+      span{
+        display inline-block
+        height 20px
+        width 20px
+        position relative
+        top 5px
+      }
+      input{
+        margin-left 10px 
+        height 30px
+        width 100px
+      }
+    }
+    .mobile-chart-color-l{
+      float left
+      span{
+        background #ff5500
+      }
+    }
+    .mobile-chart-color-r{
+      float right
+      span {
+        background #f7c100
+      }
+    }
+    .mobile-chart-box{
+      height 250px
+      width 100%
+    }
+
+    .b{
+      border-bottom 1px solid #e1e1e1
+    }
+    .r{
+      border-right 1px solid #e1e1e1
+    }
+    .color-red{
+      color red
+    }
+    .olor-blue{
+      color #1D8CE0;
+    }
+  }
 </style>
