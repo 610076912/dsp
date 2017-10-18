2af62f108a6cc3e9ebacd9795ca1c08f6290a1fc
Abeast <772325766@qq.com>
2017-10-18 11:07:33 +0800

    #0015，添加input输入数据

--- src/components/template/tpl-relation2.vue
+++ src/components/template/tpl-relation2.vue
@@ -9,11 +9,11 @@
         <div class="relation">
           <div class="ad-style" v-show="!isEdit"><img src="../../../static/img/tplbg.jpg" alt=""></div>
           <div class="ad-edit">
-          <!-- 编辑区域 -->
+            <!-- 提示模版 -->
             <div class="tpl-tips">
               <div class="tips-select">
                 <template>
-                  <el-select class="select-self" v-model="value" size="small" placeholder="请选择">
+                  <el-select class="select-self" v-model="value" size="small" @change="selectSure(value)" placeholder="请选择">
                     <el-option
                       v-for="item in options"
                       :key="item.value"
@@ -22,11 +22,21 @@
                     </el-option>
                   </el-select>
                 </template>
-              </div>
+              </div>    
               <div class="selectOption" v-if="value === 'prompt2'">
-                <div class="circular">点击上传</br>图片</div>
+                <!-- <div class="circular">点击上传</br>图片</div> -->           
+                <el-upload 
+                  :headers='headers'
+                  class="avatar-uploader circular" 
+                  action="http://192.168.1.180:5000/upload/image" 
+                  :show-file-list="false" 
+                  :on-success="handleAvatarSuccess" 
+                  :before-upload="beforeAvatarUpload">
+                  <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                  <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                </el-upload>
                 <div class="info">
-                  <el-input size="small" v-model="promptText" placeholder="请输入内容"></el-input>
+                  <el-input size="small" v-model="conf_info.prompt_info.content[0].info_con" placeholder="请输入内容"></el-input>
                 </div>
               </div>
               <div v-else-if="value === 'prompt1'">
@@ -34,8 +44,8 @@
               </div>
             </div>
 
+            <!-- 右边展示模版 -->
             <div class="tpl-con">
-              <!-- 右边模版 -->
               <div class="pic">点击上传图片</div>
               <el-input
                 class="title"
@@ -43,13 +53,13 @@
                 type="textarea"
                 :max="2"
                 placeholder="请输入内容"
-                v-model="textarea">
+                v-model="conf_info.relation_info.content[1].info_con">
               </el-input>
               <div class="priceRow">
-                <span>价格：￥</span><el-input class="price" size="small" v-model="price" placeholder="请输入内容"></el-input>
+                <span>价格：￥</span><el-input class="price" size="small" v-model="conf_info.relation_info.content[2].info_con" placeholder="请输入内容"></el-input>
               </div>
               <div class="stockRow">
-                <span>库存：</span><el-input class="num" size="small" v-model="stock" placeholder="请输入内容"></el-input>
+                <span>库存：</span><el-input class="num" size="small" v-model="conf_info.relation_info.content[3].info_con" placeholder="请输入内容"></el-input>
               </div>
               <div class="tpl-bottom">
                 <span>扫码加入购物车</span>
@@ -71,6 +81,10 @@
     },
     data () {
       return {
+        headers: {
+          Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiU2tEdnV0UkZiSDFsUHckWUFZQCIsInVzZXJfbmFtZSI6ImRzcDAwMiIsInVzZXJfdHlwZSI6IjEwMDAwMDAwIiwiaWF0IjoxNTA4MjkzMTY2LCJleHAiOjE1MjEyNTMxNjZ9.6reM7aakaBkhuZXikmyiQwy-3tDK4o8o9d3TYNDVZUw'
+        },
+        imageUrl: '',
         isEdit: false,
         btnIsShow: true,
         options: [{
@@ -91,14 +105,34 @@
             efect: '',  // 展示效果 图片：effect1，图文：effect2
             content: [{
               info_con: '',
-              info_exp: ''
+              info_exp: '提示文字'
+            },
+            {
+              info_con: '',
+              info_exp: '提示图片'
             }]
           },
           relation_info: {
             type: 'relation2',   // relation1
             content: [{
               info_con: '',
-              info_exp: ''
+              info_exp: '主图片'
+            },
+            {
+              info_con: '',
+              info_exp: '文本域'
+            },
+            {
+              info_con: '',
+              info_exp: '价格'
+            },
+            {
+              info_con: '',
+              info_exp: '库存'
+            },
+            {
+              info_con: '',
+              info_exp: '二维码'
             }]
           }
         }
@@ -114,10 +148,38 @@
       edit () {
         this.isEdit = true
       },
+      // 选定选项
+      selectSure (value) {
+        if (value === 'prompt1') {
+          this.conf_info.prompt_info.efect = 'effect1'
+          this.conf_info.prompt_info.type = 'prompt1'
+        } else if (value === 'prompt2') {
+          this.conf_info.prompt_info.efect = 'effect2'
+          this.conf_info.prompt_info.type = 'prompt2'
+        }
+        // console.log(this.conf_info)
+      },
       // 保存
       flashSave () {
         // 调父组件的save方法，并把数据传过去。
         this.$parent.save('image', this.conf_info)
+      },
+      handleAvatarSuccess (res, file) {
+        console.log(res)
+        console.log(file)
+        this.imageUrl = URL.createObjectURL(file.raw)
+      },
+      beforeAvatarUpload (file) {
+        const isJPG = file.type === 'image/jpeg'
+        const isLt2M = file.size / 1024 / 1024 < 2
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!')
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!')
+        }
+        return isJPG && isLt2M
       }
     }
   }
@@ -158,8 +220,12 @@
       z-index: 1;
       text-align: center;
       line-height: 20px;
-      padding-top: 20px
       cursor: pointer;
+      overflow hidden
+      div, img{
+        height 100%
+        width: 100%
+      }
     }
     .info {
       width: 160px;
@@ -229,8 +295,9 @@
         background: #fff;
         color: #000;
         text-align: center;
-        line-height: 50px;
+        line-height: 100px;
         float: right;
+        cursor pointer
       }
     }
   }
